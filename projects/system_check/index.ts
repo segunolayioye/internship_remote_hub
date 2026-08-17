5a03ba2f2d7c368793a5873fbf505ca05bce7d39
Segun Olayioye <segunolayioye17@gmail.com>
2026-08-17 23:58:21 +0100

    feat: implement initial type-safe trainee profile script

---
+++ projects/system_check/index.ts
@@ -0,0 +1,9 @@
+// 1. My First Type-Safe Engineering Profile
+let engineerName: string = "Olayioye OLuwasegun Isaac"
+let assignedTerminalId: number = 101;
+let readyToBuild: boolean = true;
+
+// 2. Initializing the Terminal Control Output
+console.log("Sytem initialization successful for Engineer: " + engineerName);
+console.log("Assigned Terminal ID: " + assignedTerminalId);
+console.log("Ready to build? status = " + readyToBuild);
